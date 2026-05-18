d631b320e7b9e13fa711c5c23dbb3064a0cbd582
Yarin-Shitrit <yshitrit123@gmail.com>
2026-05-18 11:28:58 +0300

    seperate queries

--- src/routes/alerts.py
+++ src/routes/alerts.py
@@ -182,6 +182,40 @@ def fetch_alert_facet_fields(
     )
     return fields
 
+@router.post(
+    path="/query/count",
+    description="Get last alerts count",
+)
+def query_alerts_count(
+    query: QueryDto,
+    authenticated_entity: AuthenticatedEntity = Depends(
+        IdentityManagerFactory.get_auth_verifier(["read:alert"])
+    ),
+):
+    tenant_id = authenticated_entity.tenant_id
+    logger.info(
+        msg="Fetching alerts count from DB",
+        extra={"tenant_id": tenant_id, "cel_expression": query.cel},
+    )
+
+    try:
+        total_count = query_total_alerts_count(tenant_id=tenant_id, query=query)
+        logger.info(
+            msg="Fetched alerts from DB",
+            extra={
+                "tenant_id": tenant_id,
+                "query": query,
+                "total_count": total_count,
+            },
+        )
+        return total_count
+
+    except CelToSqlException as e:
+        logger.exception(f'Error parsing CEL expression "{query.cel}". {str(e)}')
+        raise HTTPException(
+            status_code=400, detail=f"Error parsing CEL expression: {query.cel}"
+        ) from e
+
 
 @router.post(
     "/query",
@@ -206,7 +240,7 @@ def query_alerts(
     )
 
     try:
-        db_alerts, total_count = query_last_alerts(tenant_id=tenant_id, query=query)
+        db_alerts = query_last_alerts(tenant_id=tenant_id, query=query)
     except CelToSqlException as e:
         logger.exception(f'Error parsing CEL expression "{query.cel}". {str(e)}')
         raise HTTPException(
@@ -222,14 +256,14 @@ def query_alerts(
         extra={
             "tenant_id": tenant_id,
             "query": query,
-            "total_count": total_count,
+            "total_count": len(db_alerts),
         },
     )
 
     return {
         "limit": query.limit,
         "offset": query.offset,
-        "count": total_count,
+        "count": len(db_alerts),
         "results": enriched_alerts_dto,
     }
 
